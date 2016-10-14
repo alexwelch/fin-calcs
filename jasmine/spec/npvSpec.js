bd7294380bff981f41794ec9fa9f30d5519f4b11
Alex Welch <me@alexwelch.com>
2016-10-13 22:37:09 -0600

    NPV calculation works

--- jasmine/spec/npvSpec.js
+++ jasmine/spec/npvSpec.js
@@ -1,58 +1,32 @@
 describe("Player", function() {
-  var player;
-  var song;
+  var npv;
 
   beforeEach(function() {
-    player = new Player();
-    song = new Song();
+    npv = new window.NPV(100, .10, -1, 3, 1);
   });
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
+  describe("revenues", function() {
+    it("collects revenue across terms", function() {
+      expect(npv.revenues()).toEqual([10,19,36.1]);
     });
   });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
+  describe("expenses", function() {
+    it("collects expenses across terms", function() {
+      expect(npv.expenses()).toEqual([-1, -1.01, -1.03]);
     });
   });
+
+  describe("cashFlows", function() {
+    it("collects cashFlows across terms", function() {
+      expect(npv.cashFlows()).toEqual([9, 17.99, 35.07]);
+    });
+  });
+
+  describe("calculate", function() {
+    it("returns the NPV", function() {
+      expect(npv.calculate()).toEqual(52.75);
+    });
+  });
+
 });
--- npv.js
+++ npv.js
@@ -1,20 +1,81 @@
 (function(){
-  window.NPV = function(){
+  window.NPV = function(energy, unitPrice, periodicExpenses, term, annualPriceIncrease){
     var base = this;
+    base.energy = energy;
+    base.unitPrice = unitPrice;
+    base.periodicExpenses = periodicExpenses;
+    base.term = term;
+    base.annualPriceIncrease = 1 + annualPriceIncrease,
+    base.annualProductionIncrease = 1 - 0.05,
+    base.periodicExpenseIncrease = 1.015,
+    base.discountRate = 1.07;
 
     // energy - KWH
-    // price - PPA price - price per KWH
-    // annualExpenses - dollars
+    // unitPrice - PPA price - price per KWH
+    // expenses - per period - dollars
     // term - years
-    base.calculate = function(energy, price, annualExpenses, term, annualPriceIncrease){
-      // energy production degrades by .5% / year
-      // PPA price increases by x (default 3)% per year
-      // expenses increase by 1.5% per year
-      grossRevenue = energy * price
-      debugger;
-    };
-  };
+    // energy production degrades by .5% / year
+    // PPA price increases by x (default 3)% per year
+    // expenses increase by 1.5% per year
+    // grossRevenue = energy * price
 
-  (new NPV()).calculate(158000, .10, 75, 20, .03);
+    base.calculate = function() {
+      var cashFlows = base.cashFlows(),
+          npv = 0;
+      base._forTerm(function(i) {
+        var period = i + 1;
+        npv += cashFlows[i] / Math.pow(base.discountRate, period);
+      });
+      return _round(npv, 2);
+    };
+
+    base.cashFlows = function(){
+      var cashFlows = [],
+      revenues = base.revenues(),
+      expenses = base.expenses();
+
+      base._forTerm(function(i) {
+        cashFlows.push(revenues[i] + expenses[i]);
+      });
+      return cashFlows;
+    };
+
+    base.revenues = function() {
+      var revenues = [],
+          energy = base.energy,
+          unitPrice = base.unitPrice;
+
+      base._forTerm(function() {
+        revenue = energy * unitPrice;
+        revenues.push(revenue);
+        energy = energy * base.annualProductionIncrease,
+        unitPrice = unitPrice * base.annualPriceIncrease;
+      });
+
+      return revenues;
+    };
+
+    base.expenses = function() {
+      var expenses = [];
+      var expense = base.periodicExpenses;
+
+      base._forTerm(function(i) {
+        expenses.push(expense);
+        expense = _round(expense * base.periodicExpenseIncrease, 2);
+      });
+
+      return expenses;
+    };
+
+    base._forTerm = function(func) {
+      for (i = 0; i < base.term; i++) {
+        func.call(base, i);
+      }
+    };
+
+    function _round(value, decimals) {
+      return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+    }
+  };
 })();
 
